d17c21dbbb2904d028540d308c91a515d88632ba
asfades <serheysy@gmail.com>
2018-03-16 18:54:18 +0200

    Welcome screen v1.0

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,97 +1,72 @@
-const canvas = document.getElementById('draw')
-const context = canvas.getContext('2d')
-const sequenceLength = 200;
-
-let isDrawing = false
-let lastX = 0
-let lastY = 0
-let hue = Math.random() * 360
-let sequence = []
-let rainbow = []
-
-for (var i = 0; i < sequenceLength; i++) {
-  rainbow.push(i * 360 / sequenceLength)
-}
-
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-context.fillStyle = `hsl(${hue}, 90%,50%)`
-
-function draw (e) {
-  if (!isDrawing) return
-
-  hue = Math.random() * 360
-  
-  if (sequence.length >= sequenceLength) {
-    sequence.shift()
-  }
-  sequence.push({x: lastX, y: lastY, r: Math.random() * 15 + 25})
-  
-  clearScene();
-  sequence.forEach((item, index) => {
-    context.fillStyle = `hsl(${rainbow[index]}, 90%,50%)`
-    context.beginPath()
-    context.lineTo(item.x, item.y)
-    context.arc(item.x, item.y, item.r, 0, Math.PI * 2)
-    context.closePath()
-    context.fill()
-  })
-  
-  lastX = e.clientX
-  lastY = e.clientY
-
-  console.log(hue)
-}
-
-function toggleDrawing (e) {
-  isDrawing = !isDrawing
-}
-
-function clearScene () {
-  context.clearRect(0, 0, canvas.width, canvas.height)
-}
-
-canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mousedown', (e) => {
-  lastX = e.clientX
-  lastY = e.clientY
-  isDrawing = true
+window.addEventListener('DOMContentLoaded', function () {
+  initWelcomeScreen()
+  initDrawer()
 })
-canvas.addEventListener('mouseup', () => isDrawing = false)
 
-// let Scene = {
-//   canv: document.getElementById('scene'),
-//   chain: [],
-//   render () {
-//     this.chain.forEach((item, index) => {
-//       if (item !== undefined) {
-        
-//       }
-//     })
-//   },
-//   paint () {
-//     let context = this.canv.getContext('2d')
-//     if (this.canv.getContext) {
-//       context.fillStyle = '#4B709A'
-//       context.beginPath()
-//       let i = 0
+initWelcomeScreen = () => {
+  const welcomeScreen = document.getElementsByClassName('welcome-screen')[0]
+  const startButton = document.getElementsByClassName('welcome-screen__info-button')[0]
+  
+  startButton.addEventListener('click', function (e) {
+    welcomeScreen.style.display = 'none'
+  })
+}
 
-//       let pIntervalId = setInterval(() => {
-//         console.log(context)
-//         if (i < 100) {
-//           context.arc(20 * i, 15, 15, 0, Math.PI * 2, true)
-//           console.log(i)
-//         } else {
-//           clearInterval(pIntervalId)
-//           console.log('done')
-//         }
-//         context.fill()
+function initDrawer () {
+  const canvas = document.getElementById('draw')
+  const context = canvas.getContext('2d')
+  const sequenceLength = 200;
 
-//         i++
-//       }, 100)
-//     }
-//   }
-// }
+  let isDrawing = false
+  let lastX = 0
+  let lastY = 0
+  let hue = Math.random() * 360
+  let sequence = []
+  let rainbow = []
 
-// Scene.paint()
+  for (var i = 0; i < sequenceLength; i++) {
+    rainbow.push(i * 360 / sequenceLength)
+  }
 
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  context.fillStyle = `hsl(${hue}, 90%,50%)`
+
+  function draw (e) {
+    if (!isDrawing) return
+
+    hue = Math.random() * 360
+    
+    if (sequence.length >= sequenceLength) {
+      sequence.shift()
+    }
+    sequence.push({x: lastX, y: lastY, r: Math.random() * 15 + 25})
+    
+    clearScene();
+    sequence.forEach((item, index) => {
+      context.fillStyle = `hsl(${rainbow[index]}, 90%,50%)`
+      context.beginPath()
+      context.lineTo(item.x, item.y)
+      context.arc(item.x, item.y, item.r, 0, Math.PI * 2)
+      context.closePath()
+      context.fill()
+    })
+    
+    lastX = e.clientX
+    lastY = e.clientY
+
+    console.log(hue)
+  }
+
+  canvas.addEventListener('mousemove', draw)
+  canvas.addEventListener('mousedown', (e) => {
+    lastX = e.clientX
+    lastY = e.clientY
+    isDrawing = true
+  })
+  canvas.addEventListener('mouseup', () => isDrawing = false)
+
+  function clearScene () {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+  }
+}
